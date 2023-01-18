a5711b600ff61dc7364469d924920e3669747265
Zhaoshan-Duan <joshua.duan47@gmail.com>
2023-01-18 00:06:06 -0800

    add Stack

---
+++ src/main/kotlin/dataStructure/stack/StackImplementation.kt
@@ -0,0 +1,20 @@
+package dataStructure.stack
+
+class StackImplementation<T: Any> : Stack<T>{
+    private var storage = arrayListOf<T>()
+
+    override val count: Int
+        get() = storage.size
+
+    override fun peek(): T? {
+        return storage.lastOrNull()
+    }
+
+    override fun pop(): T? {
+        return storage.removeLastOrNull()
+    }
+
+    override fun push(element: T) {
+        storage.add(element)
+    }
+}
