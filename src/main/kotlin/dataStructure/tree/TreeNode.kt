0c322ce6d31177d5e4350a9e59881b252b0676a2
Zhaoshan-Duan <joshua.duan47@gmail.com>
2023-02-09 19:29:57 -0800

    Tree

---
+++ src/main/kotlin/dataStructure/tree/TreeNode.kt
@@ -0,0 +1,5 @@
+package dataStructure.tree
+class TreeNode<T> (val value: T) {
+    private val children: MutableList<TreeNode<T>> = mutableListOf()
+    fun add(child: TreeNode<T>) = children.add(child)
+}
